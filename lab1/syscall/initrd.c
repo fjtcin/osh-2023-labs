ac0f07fe61efc74b0ca5df7f3d41f7a6045c48d7
fjtcin <fjtcinzxh@gmail.com>
2023-03-31 05:14:48 -0700

    docs(lab1): more comments in initrd.c

--- lab1/syscall/initrd.c
+++ lab1/syscall/initrd.c
@@ -8,7 +8,11 @@ int main() {
 	if (syscall(548, buf, 33)) {
 		puts("Error: Buffer length NOT enough");
 		return -1;
+		// return -1 will cause this message:
+		// end Kernel panic - not syncing: Attempted to kill init! exitcode=0x0000ff00
 	}
 	printf("%s", buf);
 	return 0;
+	// return 0 will cause this message:
+	// end Kernel panic - not syncing: Attempted to kill init! exitcode=0x00000000
 }
