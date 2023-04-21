c47c31b7d1ca09f36129b299c1819aff0926fafa
fjtcin <fjtcinzxh@gmail.com>
2023-04-21 08:26:13 -0700

    refactor: execute command

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -15,8 +15,9 @@
 //getenv
 #include <stdlib.h>
 
-std::vector<std::string> split(std::string s, const std::string &delimiter);
-std::vector<std::string> get_args(std::string s);
+void exec(std::string);
+std::vector<std::string> split(std::string, const std::string&);
+std::vector<std::string> get_args(std::string);
 
 int main() {
   // 不同步 iostream 和 cstdio 的 buffer
@@ -31,89 +32,93 @@ int main() {
     // 读入一行。std::getline 结果不包含换行符。
     std::getline(std::cin, cmd);
 
-    // 按空格分割命令为单词
-    std::vector<std::string> args = get_args(cmd);
+    exec(cmd);
+  }
+}
 
-    // 没有可处理的命令
-    if (args.empty()) {
-      continue;
+void exec(std::string cmd) {
+  // 按空格分割命令为单词
+  std::vector<std::string> args = get_args(cmd);
+
+  // 没有可处理的命令
+  if (args.empty()) {
+    return;
+  }
+
+  // 退出
+  if (args[0] == "exit") {
+    if (args.size() <= 1) {
+      exit(0);
     }
 
-    // 退出
-    if (args[0] == "exit") {
-      if (args.size() <= 1) {
-        return 0;
-      }
+    // std::string 转 int
+    std::stringstream code_stream(args[1]);
+    int code = 0;
+    code_stream >> code;
 
-      // std::string 转 int
-      std::stringstream code_stream(args[1]);
-      int code = 0;
-      code_stream >> code;
-
-      // 转换失败
-      if (!code_stream.eof() || code_stream.fail()) {
-        std::cerr << "Invalid exit code\n";
-        continue;
-      }
-
-      return code;
+    // 转换失败
+    if (!code_stream.eof() || code_stream.fail()) {
+      std::cerr << "exit: invalid exit code\n";
+      return;
     }
 
-    if (args[0] == "pwd") {
-      if (args.size() > 1) {
-        std::cerr << "pwd: too many arguments\n";
-        continue;
-      }
-      char buf[PATH_MAX];
-      if (getcwd(buf, PATH_MAX)) {
-        std::cout << buf << '\n';
-      } else {
-        std::cerr << "pwd: failed\n";
-      }
-      continue;
+    exit(code);
+  }
+
+  if (args[0] == "pwd") {
+    if (args.size() > 1) {
+      std::cerr << "pwd: too many arguments\n";
+      return;
     }
-
-    if (args[0] == "cd") {
-      if (args.size() > 2) {
-        std::cerr << "cd: too many arguments\n";
-        continue;
-      }
-      if (args.size() <= 1) {
-        if (chdir(getenv("HOME")))
-          std::cerr << "cd: failed\n";
-      } else {
-        if (chdir(args[1].c_str()))
-          std::cerr << "cd: no such file or directory: " << args[1] << '\n';
-      }
-      continue;
+    char buf[PATH_MAX];
+    if (getcwd(buf, PATH_MAX)) {
+      std::cout << buf << '\n';
+    } else {
+      std::cerr << "pwd: failed\n";
     }
+    return;
+  }
 
-    // 处理外部命令
-    pid_t pid = fork();
-
-    // std::vector<std::string> 转 char **
-    char *arg_ptrs[args.size() + 1];
-    for (auto i = 0; i < args.size(); i++) {
-      arg_ptrs[i] = &args[i][0];
+  if (args[0] == "cd") {
+    if (args.size() > 2) {
+      std::cerr << "cd: too many arguments\n";
+      return;
     }
-    // exec p 系列的 argv 需要以 nullptr 结尾
-    arg_ptrs[args.size()] = nullptr;
-
-    if (pid == 0) {
-      // 这里只有子进程才会进入
-      // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
-      // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
-      execvp(args[0].c_str(), arg_ptrs);
-
-      // 所以这里直接报错
-      exit(255);
+    if (args.size() <= 1) {
+      if (chdir(getenv("HOME")))
+        std::cerr << "cd: failed\n";
+    } else {
+      if (chdir(args[1].c_str()))
+        std::cerr << "cd: no such file or directory: " << args[1] << '\n';
     }
+    return;
+  }
 
-    // 这里只有父进程（原进程）才会进入
-    int ret = wait(nullptr);
-    if (ret < 0) {
-      std::cerr << "wait failed";
-    }
+  // 处理外部命令
+  pid_t pid = fork();
+
+  // std::vector<std::string> 转 char **
+  char *arg_ptrs[args.size() + 1];
+  for (auto i = 0; i < args.size(); i++) {
+    arg_ptrs[i] = &args[i][0];
+  }
+  // exec p 系列的 argv 需要以 nullptr 结尾
+  arg_ptrs[args.size()] = nullptr;
+
+  if (pid == 0) {
+    // 这里只有子进程才会进入
+    // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
+    // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
+    execvp(args[0].c_str(), arg_ptrs);
+
+    // 所以这里直接报错
+    exit(255);
+  }
+
+  // 这里只有父进程（原进程）才会进入
+  int ret = wait(nullptr);
+  if (ret < 0) {
+    std::cerr << "wait failed";
   }
 }
 
