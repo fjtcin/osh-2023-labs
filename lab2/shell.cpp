3ec35ad86d23fcfb925be4568b0ed3518ac7bfac
fjtcin <fjtcinzxh@gmail.com>
2023-04-21 23:08:54 -0700

    fix(lab2): implement <

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -12,15 +12,19 @@
 #include <unistd.h>
 // wait
 #include <sys/wait.h>
-//getenv
-#include <stdlib.h>
+// std::size_t, std::getenv
+#include <cstdlib>
 // open
 #include <fcntl.h>
 
+#define REDIRECT_MSG "redirect: not a redirection or syntex not supported, no redirection processed\n"
+
 void exec(std::string);
 void exec_pipe(std::vector<std::string>);
+void redirect(std::vector<std::string>&);
 std::vector<std::string> split(std::string, const std::string&);
-std::vector<std::string> get_args(std::string);
+std::vector<std::string> get_args(const std::string&);
+int st2i(const std::string&);
 
 int main() {
   // 不同步 iostream 和 cstdio 的 buffer
@@ -93,7 +97,7 @@ void exec(std::string cmd) {
       return;
     }
     if (args.size() <= 1) {
-      if (chdir(getenv("HOME")))
+      if (chdir(std::getenv("HOME")))
         std::cerr << "cd: failed\n";
     } else {
       if (chdir(args[1].c_str()))
@@ -104,17 +108,21 @@ void exec(std::string cmd) {
 
   // 处理外部命令
   pid_t pid = fork();
-
-  // std::vector<std::string> 转 char **
-  char *arg_ptrs[args.size() + 1];
-  for (size_t i = 0; i < args.size(); i++) {
-    arg_ptrs[i] = &args[i][0];
+  if (pid < 0) {
+    std::cerr << "fork failed\n";
+    return;
   }
-  // exec p 系列的 argv 需要以 nullptr 结尾
-  arg_ptrs[args.size()] = nullptr;
-
   if (pid == 0) {
     // 这里只有子进程才会进入
+    redirect(args);
+
+    // std::vector<std::string> 转 char **
+    char *arg_ptrs[args.size() + 1];
+    for (std::size_t i = 0; i < args.size(); i++)
+      arg_ptrs[i] = &args[i][0];
+    // exec p 系列的 argv 需要以 nullptr 结尾
+    arg_ptrs[args.size()] = nullptr;
+
     // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
     // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
     execvp(args[0].c_str(), arg_ptrs);
@@ -128,8 +136,9 @@ void exec(std::string cmd) {
 }
 
 void exec_pipe(std::vector<std::string> cmd_list) {
-  int num = cmd_list.size(), last_fd0;
-  for (int i = 0; i < num; i++) {
+  const auto num = cmd_list.size();
+  int last_fd0;
+  for (std::size_t i = 0; i < num; i++) {
     int fd[2];
     if (i < num - 1 && pipe(fd) < 0) {
       std::cerr << "pipe failed\n";
@@ -167,11 +176,71 @@ void exec_pipe(std::vector<std::string> cmd_list) {
   }
 }
 
+void redirect(std::vector<std::string>& args) {
+  int fd, fd2;
+  for (auto it = args.begin(); it != args.end();) {
+    auto i = it->find("<");
+    if (i != std::string::npos) {
+      if (i == 0) {
+        fd2 = 0;
+      } else if ((fd2 = st2i(it->substr(0, i))) == -1) {
+        std::cerr << REDIRECT_MSG;
+        ++it;
+        continue;
+      }
+      const auto arg = it->substr(i+1);
+      if (arg.size() == 2) {
+        if (arg[0] == '<' && arg[1] == '<') {
+          ++it;
+        } else {
+          std::cerr << REDIRECT_MSG;
+          ++it;
+        }
+      } else if (arg.size() == 1) {
+        if (arg[0] == '<') {
+          ++it;
+        } else {
+          std::cerr << REDIRECT_MSG;
+          ++it;
+        }
+      } else if (arg.empty()) {
+        it = args.erase(it);
+        if ((fd = open(it->c_str(), O_RDONLY)) < 0) {
+          std::cerr << "open failed\n";
+          args.clear();
+          return;
+        };
+        if (dup2(fd, fd2) < 0) {
+          std::cerr << "dup2 failed\n";
+          args.clear();
+          return;
+        }
+        if (close(fd) < 0) {
+          std::cerr << "close failed\n";
+          args.clear();
+          return;
+        }
+        it = args.erase(it);
+      } else {
+        ++it;
+        std::cerr << REDIRECT_MSG;
+      }
+      continue;
+    }
+    i = it->find(">");
+    if (i != std::string::npos) {
+      ++it;
+      continue;
+    }
+    ++it;
+  }
+}
+
 // 经典的 cpp string split 实现
 // https://stackoverflow.com/a/14266139/11691878
-std::vector<std::string> split(std::string s, const std::string &delimiter) {
+std::vector<std::string> split(std::string s, const std::string& delimiter) {
   std::vector<std::string> res;
-  size_t pos = 0;
+  std::size_t pos = 0;
   std::string token;
   while ((pos = s.find(delimiter)) != std::string::npos) {
     token = s.substr(0, pos);
@@ -182,10 +251,21 @@ std::vector<std::string> split(std::string s, const std::string &delimiter) {
   return res;
 }
 
-std::vector<std::string> get_args(std::string s) {
+std::vector<std::string> get_args(const std::string& s) {
   std::vector<std::string> res;
   std::string buf;
   std::stringstream ss(s);
   while (ss >> buf) res.push_back(buf);
   return res;
+}
+
+int st2i(const std::string& s) {
+  try {
+    std::size_t pos;
+    int num = std::stoi(s, &pos);
+    if (pos == s.size()) return num;
+    return -1;
+  } catch (...) {
+    return -1;
+  }
 }
