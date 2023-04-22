5433fb4f417a4fc15bf10bdf0a0a9d51a5dcd40a
fjtcin <fjtcinzxh@gmail.com>
2023-04-22 00:38:00 -0700

    build(lab2): implement >>

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -217,7 +217,7 @@ void redirect(std::vector<std::string>& args) {
           std::cerr << "open failed\n";
           args.clear();
           return;
-        };
+        }
         if (dup2(fd, fd2) < 0) {
           std::cerr << "dup2 failed\n";
           args.clear();
@@ -246,40 +246,32 @@ void redirect(std::vector<std::string>& args) {
         return;
       }
       const auto arg = it->substr(i+1);
-      if (arg.size() == 1) {
-        if (arg[0] == '>') {
-          ++it;
-        } else {
-          std::cerr << "redirect failed\n";
-          args.clear();
-          return;
-        }
-      } else if (arg.empty()) {
+      if ((arg.size() == 1 && arg[0] == '>') || arg.empty()) {
         it = args.erase(it);
         if (it == args.end()) {
           std::cerr << "redirect failed\n";
           args.clear();
           return;
         }
-        if ((fd = open(it->c_str(), O_WRONLY | O_CREAT | O_TRUNC,
+        if ((fd = open(it->c_str(), O_WRONLY | O_CREAT | (arg.size() == 1 ? O_APPEND : O_TRUNC),
             S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0) { // -rw-r--r--
           std::cerr << "open failed\n";
-          args.clear();
-          return;
-        };
-        if (dup2(fd, fd2) < 0) {
-          std::cerr << "dup2 failed\n";
-          args.clear();
-          return;
-        }
-        if (close(fd) < 0) {
-          std::cerr << "close failed\n";
           args.clear();
           return;
         }
         it = args.erase(it);
       } else {
         std::cerr << "redirect failed\n";
+        args.clear();
+        return;
+      }
+      if (dup2(fd, fd2) < 0) {
+        std::cerr << "dup2 failed\n";
+        args.clear();
+        return;
+      }
+      if (close(fd) < 0) {
+        std::cerr << "close failed\n";
         args.clear();
         return;
       }
